a55b527c9c91a89ec78488f7ba6b4a82be2d3ceb
Dennis <denniskamau3@gmail.com>
2019-09-03 22:58:37 +0300

    add login form

---
+++ src/components/Registration/Login/LoginForm.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Field } from 'redux-form';
+import formcomponent from '../formcomponent'
+export const LoginForm = ({ handleSubmit, onSubmit, props }) => {
+
+
+
+
+  return (
+    <div>
+      <h1>login</h1>
+
+      <form
+        className="loginForm"
+        onSubmit={handleSubmit(onSubmit)}
+      >
+      <Field name="email" label="Email" component={formcomponent} type="email" placeholder="Email" />
+      <Field name="password" label="Password" component={formcomponent} type="password" placeholder="Password" />
+
+        <button type="submit" className="loginBtn">
+          Submit
+        </button>
+      </form>
+
+    </div>
+  );
+}
+
+export default LoginForm
